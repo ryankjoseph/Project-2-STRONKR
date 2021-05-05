9c1d0f3653fc36c826d98b6b54394f8c21daf5a4
Ryan Joseph <ryankjoseph@gmail.com>
2021-05-05 17:08:00 -0400

    Add data for seeds to use

---
+++ project-2/data.js
@@ -0,0 +1,21 @@
+
+  exports.activityPhases=[
+    {phase: "Beginner" ,set: 3 , repetitions: 10, daysToUpgrade: 21},
+    {phase: "Intermediate" ,set: 5 , repetitions: 15 , daysToUpgrade: 24},
+    {phase: "Advanced" ,set: 7 , repetitions: 20 , daysToUpgrade: 30},
+    {phase: "Pro" ,set: 10 , repetitions: 25 , daysToUpgrade: 34},
+    {phase: "STRONKR" ,set: 12 , repetitions: 25 , daysToUpgrade: 44},
+  ]
+
+  exports.activities=[
+    {name: "Lunges" ,focus: ["Legs"] , video: "https://www.youtube.com/watch?v=QOVaHwm-Q6U"},
+    {name: "Archer Push Ups" ,focus: ["Biceps", "Triceps"] , video: "https://www.youtube.com/watch?v=ZLWko5aP1FM"},
+    {name: "Diamond Push Ups" ,focus: ["Triceps","Core"] , video: "https://www.youtube.com/watch?v=J0DnG1_S92I"},
+    {name: "Pull Ups" ,focus: ["Lats"] , video: "https://www.youtube.com/watch?v=eGo4IYlbE5g"},
+    {name: "Chin Ups" ,focus: ["Biceps"] , video: "https://www.youtube.com/watch?v=b-ztMQpj8yc"},
+    {name: "Sit Ups" ,focus: ["Core"] , video: "https://www.youtube.com/watch?v=1fbU_MkV7NE"},
+    {name: "Plank" ,focus: ["Core"] , video: "https://www.youtube.com/watch?v=pSHjTRCQxIw"},
+    {name: "Side Plank" ,focus: ["Core"] , video: "https://www.youtube.com/watch?v=9Q0D6xAyrOI"},
+    {name: "Oblique Crunch" ,focus: ["Legs","Core"] , video: "https://www.youtube.com/watch?v=QxaKQG-gmPY"},
+    {name: "Squat" ,focus: ["Legs","Core"] , video: "https://www.youtube.com/watch?v=UXJrBgI2RxA"},
+  ]
